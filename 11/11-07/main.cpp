b039facf52062cf8646e8f35fd3c6a19838f185d
Ruslan Slobodian <ruslan@tk.te.ua>
2023-10-24 21:03:59 +0300

    10th chapter refactored

--- 11/11-07/main.cpp
+++ 11/11-07/main.cpp
@@ -1,28 +1,24 @@
-//Код програми 11.7. Демонстрація механізму використання бібліотечної функції
-//qsort() для сортування елементів цілочисельного масиву
+// Код програми 11.7. Демонстрація механізму використання бібліотечної функції qsort()
+// для сортування елементів цілочисельного масиву
 
 
-#include <iostream>		 		// Для потокового введення-виведення
-#include <cstdlib>				// Стандартна бібліотека С++
+#include <iostream>
+#include <cstdlib>
 
-using namespace std; 			// Використання стандартного простору імен
+using namespace std;
 
-int Comp(const void *a, const void *b)
-{
-    return * (char *) a – * (char *) b;
+int comp(const void* a, const void* b) {
+    return *(char*) a - *(char*) b;
 }
 
-
-int main()
-{
+int main() {
     int num[] = {10, 4, 3, 6, 5, 7, 8};
 
-    qsort(num, 7, sizeof(int), Comp);
+    qsort(num, 7, sizeof(int), comp);
 
-    for(int i=0; i<7; i++)
-        cout << num[i]<< " ";
+    for (int i = 0; i < 7; i++)
+        cout << num[i] << " ";
     cout << endl;
-
 
     //system("PAUSE");
     return EXIT_SUCCESS;
